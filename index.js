35e7330a4c646bae7461094681bab3620240cb5a
Arpan Wasti <wastiarpan7@gmail.com>
2023-10-01 12:58:42 +0545

    hehe

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //GeoLocation
 var x = document.getElementById("currentLocation");
+
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
@@ -7,17 +8,17 @@ function getLocation() {
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
-
 function showPosition(position) {
   x.innerHTML = "Latitude: " + position.coords.latitude +
   "<br>Longitude: " + position.coords.longitude;
 }
  
 
-let map;
 
+
+//showMap
+let map;
 function initMap() {
- 
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat:27.67573106538192, lng:85.34534985238629 },
     zoom: 15,
@@ -29,4 +30,25 @@ const marker = new google.maps.Marker({
     map: map,
 });
 
+}
+
+
+//creating DirectionService function
+var directionsService = new google.maps.DirectionsService();
+//creating DirectionRenderer function
+var directionsDisplay = new google.maps.DirectionsRenderer();
+//binding direction
+directionsDisplay.setMap(map);
+
+function calcRoute(){
+        //create request
+
+    var request={
+      origin: document.getElementById("from").value,
+      destination: document.getElementById("to").value,
+      travelMode: google.maps.TravelMode.DRIVING,// WALKING , BYCYCLING, TRANSIT
+      unitSystem: google.maps.UnitSystem.metric
+  }
+  
+  
 }
