afecf4049bcf2c48d47b58daa5ea0b40f12f9e43
aayush <aayusshgh@gmail.com>
2023-10-02 20:07:49 +0545

    test commit

--- index.js
+++ index.js
@@ -10,26 +10,29 @@ function getLocation() {
 }
 
 function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude;
- 
+  x.innerHTML =
+    "Latitude: " +
+    position.coords.latitude +
+    "<br>Longitude: " +
+    position.coords.longitude;
+
   const marker = new google.maps.Marker({
     position: getLocation,
     map: map,
-});
-map.setCenter(getLocation);
-
+  });
+  map.setCenter(getLocation);
 }
+
+// This is just a change
 
 //showMap
 let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat:27.6999855, lng:85.3278716 },
+    center: { lat: 27.6999855, lng: 85.3278716 },
     mapId: "1aea3bc268f46967",
     zoom: 13.2,
   });
-
 }
 
 //creating DirectionService function
@@ -39,15 +42,13 @@ var directionsDisplay = new google.maps.DirectionsRenderer();
 //binding direction
 directionsDisplay.setMap(map);
 
-function calcRoute(){
-        //create request
+function calcRoute() {
+  //create request
 
-    var request={
-      origin: document.getElementById("from").value,
-      destination: document.getElementById("to").value,
-      travelMode: google.maps.TravelMode.DRIVING,// WALKING , BYCYCLING, TRANSIT
-      unitSystem: google.maps.UnitSystem.metric
-  }
-  
-  
+  var request = {
+    origin: document.getElementById("from").value,
+    destination: document.getElementById("to").value,
+    travelMode: google.maps.TravelMode.DRIVING, // WALKING , BYCYCLING, TRANSIT
+    unitSystem: google.maps.UnitSystem.metric,
+  };
 }
