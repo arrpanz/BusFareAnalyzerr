ceb3c1241349cad6554a5f4ea0c3b4aaec61fc53
aayush <aayusshgh@gmail.com>
2023-10-02 22:37:28 +0545

    Route Show

--- index.js
+++ index.js
@@ -1,54 +1,38 @@
-//GeoLocation
-var x = document.getElementById("currentLocation");
-
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-
-function showPosition(position) {
-  x.innerHTML =
-    "Latitude: " +
-    position.coords.latitude +
-    "<br>Longitude: " +
-    position.coords.longitude;
-
-  const marker = new google.maps.Marker({
-    position: getLocation,
-    map: map,
-  });
-  map.setCenter(getLocation);
-}
-
-// This is just a change
-
 //showMap
-let map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
+  new google.maps.Map(document.getElementById("map"), {
     center: { lat: 27.6999855, lng: 85.3278716 },
     mapId: "1aea3bc268f46967",
-    zoom: 13.2,
+    zoom: 12,
   });
 }
 
-//creating DirectionService function
-var directionsService = new google.maps.DirectionsService();
-//creating DirectionRenderer function
-var directionsDisplay = new google.maps.DirectionsRenderer();
-//binding direction
-directionsDisplay.setMap(map);
+//Show route between two places
+function mapRequest(lat1, long1, lat2, long2) {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 27.6999855, lng: 85.3278716 },
+    mapId: "1aea3bc268f46967",
+    zoom: 12,
+  });
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer();
 
-function calcRoute() {
-  //create request
+  directionsRenderer.setMap(map);
 
-  var request = {
-    origin: document.getElementById("from").value,
-    destination: document.getElementById("to").value,
-    travelMode: google.maps.TravelMode.DRIVING, // WALKING , BYCYCLING, TRANSIT
-    unitSystem: google.maps.UnitSystem.metric,
+  const start = new google.maps.LatLng(lat1, long1);
+  const end = new google.maps.LatLng(lat2, long2);
+
+  const request = {
+    origin: start,
+    destination: end,
+    travelMode: "DRIVING",
   };
+
+  directionsService.route(request, function (response, status) {
+    if (status === "OK") {
+      directionsRenderer.setDirections(response);
+    } else {
+      console.error("Directions request failed:", status);
+    }
+  });
 }
