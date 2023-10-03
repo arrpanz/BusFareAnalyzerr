9f2d982e1c08c13164c2348c85a6441ac3220ce7
Arpan Wasti <wastiarpan7@gmail.com>
2023-10-03 21:28:12 +0545

    Fixed

--- index.js
+++ index.js
@@ -7,35 +7,35 @@ function initMap() {
   });
 }
 
-//Show route between two places
-function mapRequest(lat1, long1, lat2, long2) {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 27.6999855, lng: 85.3278716 },
-    mapId: "1aea3bc268f46967",
-    zoom: 12,
-  });
-  const directionsService = new google.maps.DirectionsService();
-  const directionsRenderer = new google.maps.DirectionsRenderer();
+// //Show route between two places
+// function mapRequest(lat1, long1, lat2, long2) {
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: 27.6999855, lng: 85.3278716 },
+//     mapId: "1aea3bc268f46967",
+//     zoom: 12,
+//   });
+//   const directionsService = new google.maps.DirectionsService();
+//   const directionsRenderer = new google.maps.DirectionsRenderer();
 
-  directionsRenderer.setMap(map);
+//   directionsRenderer.setMap(map);
 
-  const start = new google.maps.LatLng(lat1, long1);
-  const end = new google.maps.LatLng(lat2, long2);
+//   const start = new google.maps.LatLng(lat1, long1);
+//   const end = new google.maps.LatLng(lat2, long2);
 
-  const request = {
-    origin: start,
-    destination: end,
-    travelMode: "DRIVING",
-  };
+//   const request = {
+//     origin: start,
+//     destination: end,
+//     travelMode: "DRIVING",
+//   };
 
-  directionsService.route(request, function (response, status) {
-    if (status === "OK") {
-      directionsRenderer.setDirections(response);
-    } else {
-      console.error("Directions request failed:", status);
-    }
-  });
-}
+//   directionsService.route(request, function (response, status) {
+//     if (status === "OK") {
+//       directionsRenderer.setDirections(response);
+//     } else {
+//       console.error("Directions request failed:", status);
+//     }
+//   });
+// }
 
 //**************************************** markers ****************************************
 // List of locations with custom markers
