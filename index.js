25473199d363aea19cf1b2ece1df3ebca551474a
Arpan Wasti <wastiarpan7@gmail.com>
2023-10-04 12:42:59 +0545

    Fixed

--- index.js
+++ index.js
@@ -6,38 +6,42 @@ function initMap() {
     zoom: 13.3,
   });
 }
+//Calculation 
 
-// //Show route between two places
-// function mapRequest(lat1, long1, lat2, long2) {
-//   const map = new google.maps.Map(document.getElementById("map"), {
-//     center: { lat: 27.6999855, lng: 85.3278716 },
-//     mapId: "1aea3bc268f46967",
-//     zoom: 12,
-//   });
-//   const directionsService = new google.maps.DirectionsService();
-//   const directionsRenderer = new google.maps.DirectionsRenderer();
 
-//   directionsRenderer.setMap(map);
 
-//   const start = new google.maps.LatLng(lat1, long1);
-//   const end = new google.maps.LatLng(lat2, long2);
 
-//   const request = {
-//     origin: start,
-//     destination: end,
-//     travelMode: "DRIVING",
-//   };
+//////
+//Show route between two places
+function mapRequest(lat1, long1, lat2, long2) {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 27.6999855, lng: 85.3278716 },
+    mapId: "1aea3bc268f46967",
+    zoom: 12,
+  });
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer();
 
-//   directionsService.route(request, function (response, status) {
-//     if (status === "OK") {
-//       directionsRenderer.setDirections(response);
-//     } else {
-//       console.error("Directions request failed:", status);
-//     }
-//   });
-// }
+  const start = new google.maps.LatLng(lat1, long1);
+  const end = new google.maps.LatLng(lat2, long2);
 
-//**************************************** markers ****************************************
+  const request = {
+    origin: start,
+    destination: end,
+    travelMode: "DRIVING",
+  };
+
+  directionsService.route(request, function (response, status) {
+    if (status === "OK") {
+      directionsRenderer.setDirections(response);
+    } else {
+      console.error("Directions request failed:", status);
+    }
+    directionsRenderer.setMap(map);
+  });
+}
+
+// **************************************** markers ****************************************
 // List of locations with custom markers
 // List of locations with custom markers
 
@@ -51,4 +55,5 @@ function clearStuffs() {
 
   document.getElementById("latitude2").textContent = '';
   document.getElementById("longitude2").textContent = '';
+  
 }
