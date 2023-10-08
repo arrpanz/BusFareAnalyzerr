bb2a3134a1b67eb4c8a3e101f53ed0767b408030
aayush <aayusshgh@gmail.com>
2023-10-08 19:54:37 +0545

    live location tracking

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // Merged JavaScript code from index.html and index.js
 
 // Initialize Google Maps
+
 function initMap() {
   new google.maps.Map(document.getElementById("map"), {
     center: { lat: 27.696443, lng: 85.32458 },
     mapId: "1aea3bc268f46967",
     zoom: 13.3,
   });
- 
-  
 }
 
 // Show route function
@@ -26,7 +25,6 @@ function getRoute() {
       );
     }
   }, 2000);
-  
 }
 
 // Calculation
@@ -40,19 +38,30 @@ function calculateFare(distance) {
   const distanceInKm = (distance / 1000).toFixed(2);
   const fare = baseFare + farePerKilometer * distanceInKm;
   if (userType === "student") {
-    fareOutput.textContent = `Rs ${Math.round((fare - 0.45 * fare) * 100) / 100}`;
+    fareOutput.textContent = `Rs ${
+      Math.round((fare - 0.45 * fare) * 100) / 100
+    }`;
   } else if (userType === "senior") {
-    fareOutput.textContent = `Rs ${Math.round((fare - 0.5 * fare) * 100) / 100}`;
+    fareOutput.textContent = `Rs ${
+      Math.round((fare - 0.5 * fare) * 100) / 100
+    }`;
   } else {
     fareOutput.textContent = `Rs ${Math.round(fare * 100) / 100}`;
   }
 }
 
+const trackLocation = ({ onSuccess, onError = () => {} }) => {
+  if ("geolocation" in navigator === false) {
+    return onError(new Error("Geolocation is not supported by your browser."));
+  }
+
+  return navigator.geolocation.watchPosition(onSuccess, onError);
+};
+
 // Show route between two places
 function mapRequest(lat1, long1, lat2, long2) {
   const distanceOutput = document.getElementById("distanceOutput");
-  
-  
+
   const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 27.6999855, lng: 85.3278716 },
     mapId: "1aea3bc268f46967",
@@ -78,33 +87,6 @@ function mapRequest(lat1, long1, lat2, long2) {
     }
     directionsRenderer.setMap(map);
 
-    const steps = response.routes[0].overview_path;
-
-    const marker = new google.maps.Marker({
-          map: map,
-          position: {
-            lat: steps[0].lat(),
-            lng: steps[0].lng(),
-          },
-          label: "🚘",
-          zIndex: 1,
-        });
-    
-        let i = 0;
-        const interval = setInterval(function () {
-          i++;
-          if (i === steps.length) {
-            clearInterval(interval);
-            return;
-          }
-    
-          marker.setPosition({
-            lat: steps[i].lat(),
-            lng: steps[i].lng(),
-          });
-        }, 1000);
-
-
     const fromLatLng = new google.maps.LatLng(lat1, long1);
     const toLatLng = new google.maps.LatLng(lat2, long2);
 
@@ -116,6 +98,22 @@ function mapRequest(lat1, long1, lat2, long2) {
 
     distanceOutput.innerText = `Distance: ${(distance / 1000).toFixed(2)} kms.`;
     calculateFare(distance);
+
+    const initialPosition = { lat: Number(lat1), lng: Number(long1) };
+    const marker = new google.maps.Marker({
+      map: map,
+      position: initialPosition,
+      label: "🚘",
+      zIndex: 999,
+    });
+    trackLocation({
+      onSuccess: ({ coords: { latitude: lat, longitude: lng } }) => {
+        marker.setPosition({ lat, lng });
+        console.log({ lat, lng });
+      },
+      onError: (err) =>
+        alert(`Error: ${getPositionErrorMessage(err.code) || err.message}`),
+    });
   });
 }
 
@@ -151,77 +149,76 @@ function clearStuffs() {
   });
 }
 
-
-
 // Initializing fromValue and toValue to get value from two source and destination location
 let fromValue = document.getElementById("from").value;
 let toValue = document.getElementById("to").value;
 
 // Initializing global variables of two selectable form options
-const selectFrom = document.getElementById('from');
-const selectTo = document.getElementById('to');
+const selectFrom = document.getElementById("from");
+const selectTo = document.getElementById("to");
 
 // Listening if a value of the selectable form option has changed (if changed, initializes the latest selected value)
-selectFrom.addEventListener('change', function handleChange(e) {
-    fromValue = document.getElementById("from").value;
+selectFrom.addEventListener("change", function handleChange(e) {
+  fromValue = document.getElementById("from").value;
 });
 
-selectTo.addEventListener('change', function handleChange(e) {
-    toValue = document.getElementById("to").value;
+selectTo.addEventListener("change", function handleChange(e) {
+  toValue = document.getElementById("to").value;
 });
 
 // Get Longitude and latitude of first place
 function getLongitudeLatitude(placeName) {
-    const geocoder = new google.maps.Geocoder();
-    const latitudeOutput = document.getElementById("latitude");
-    const longitudeOutput = document.getElementById("longitude");
+  const geocoder = new google.maps.Geocoder();
+  const latitudeOutput = document.getElementById("latitude");
+  const longitudeOutput = document.getElementById("longitude");
 
-    geocoder.geocode({
-            address: placeName  //geocoding request to the Google Maps API
-        },
-        function(results,status) {
-            if (status === "OK" && results[0]) {
-                const location = results[0].geometry.location;
-                const lat = location.lat();
-                const lng = location.lng();
+  geocoder.geocode(
+    {
+      address: placeName, //geocoding request to the Google Maps API
+    },
+    function (results, status) {
+      if (status === "OK" && results[0]) {
+        const location = results[0].geometry.location;
+        const lat = location.lat();
+        const lng = location.lng();
 
-                latitudeOutput.textContent =  lat ;
-                longitudeOutput.textContent =  lng;
-            } else {
-                alert("Geocode was not successful for the following reason: " + status);
-            }
-        }
-    );
+        latitudeOutput.textContent = lat;
+        longitudeOutput.textContent = lng;
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    }
+  );
 }
 
 // Get Longitude and latitude of second place
 function getLongitudeLatitude2(placeName) {
-    const geocoder2 = new google.maps.Geocoder();
-    const latitudeOutput2 = document.getElementById("latitude2");
-    const longitudeOutput2 = document.getElementById("longitude2");
+  const geocoder2 = new google.maps.Geocoder();
+  const latitudeOutput2 = document.getElementById("latitude2");
+  const longitudeOutput2 = document.getElementById("longitude2");
 
-    geocoder2.geocode({
-            address: placeName
-        },
-        function(results, status) {
-            if (status === "OK" && results[0]) {
-                const location = results[0].geometry.location;
-                const lat2 = location.lat();
-                const lng2 = location.lng();
-                
-               latitudeOutput2.textContent = lat2;
-                longitudeOutput2.textContent = lng2;
-            } else {
-                alert("Geocode was not successful for the following reason: " + status);
-            }
-        }
-    );
+  geocoder2.geocode(
+    {
+      address: placeName,
+    },
+    function (results, status) {
+      if (status === "OK" && results[0]) {
+        const location = results[0].geometry.location;
+        const lat2 = location.lat();
+        const lng2 = location.lng();
 
+        latitudeOutput2.textContent = lat2;
+        longitudeOutput2.textContent = lng2;
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    }
+  );
 }
 
 function getLatLong() {
-    const fromValue = document.getElementById('from').value;
-    const toValue = document.getElementById('to').value;
-    getLongitudeLatitude(fromValue);
-    getLongitudeLatitude2(toValue);
+  const fromValue = document.getElementById("from").value;
+  const toValue = document.getElementById("to").value;
+  getLongitudeLatitude(fromValue);
+  getLongitudeLatitude2(toValue);
 }
